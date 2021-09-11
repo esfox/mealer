defb69a37fbb488a002ed4dd08957824d9dea427
esfox <osairis100@gmail.com>
2021-09-11 23:25:53 +0800

    Create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary white--text">
+      <v-toolbar-title class="text-h4 font-weight-bold">
+        🍱 Mealer
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+html {
+  overflow-y: auto;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,11 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    '~/components',
+    '~/views',
+    '~/layouts',
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
