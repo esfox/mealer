f4c68721126424f39c4f1ad805f034ad5cb8ad82
esfox <osairis100@gmail.com>
2021-09-12 01:23:50 +0800

    Add food view

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <div></div>
+  <v-tabs-items v-model="tab" class="tabs">
+    <v-tab-item>
+      <Food />
+    </v-tab-item>
+    <v-tab-item> hi </v-tab-item>
+  </v-tabs-items>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      tab: 0,
+    }
+  },
+  created() {
+    this.$nuxt.$on('tab-change', (tab) => (this.tab = tab))
+  },
+}
 </script>
+
+<style lang="scss">
+.tabs {
+  height: 100%;
+
+  .v-window-item {
+    height: 100%;
+  }
+}
+</style>
