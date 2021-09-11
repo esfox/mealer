d477e5d7d400f1358427bcc1e1006cab6ec97ed6
esfox <osairis100@gmail.com>
2021-09-12 01:23:44 +0800

    Add tabs

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary white--text">
+    <v-app-bar app dark extension-height="58" color="primary">
       <v-toolbar-title class="text-h4 font-weight-bold">
         🍱 Mealer
       </v-toolbar-title>
+      <template #extension>
+        <v-tabs v-model="tab" grow @change="$nuxt.$emit('tab-change', tab)">
+          <v-tab class="font-weight-bold py-4">
+            <v-icon large class="pr-3">mdi-food-fork-drink</v-icon>
+            <h2>Food</h2>
+          </v-tab>
+          <v-tab class="font-weight-bold py-4">
+            <v-icon class="pr-3">mdi-calendar-month</v-icon>
+            <h2>Meals</h2>
+          </v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
     <v-main>
       <Nuxt />
@@ -11,8 +23,22 @@
   </v-app>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      tab: 0,
+    }
+  },
+}
+</script>
+
 <style>
 html {
-  overflow-y: auto;
+  overflow-y: auto !important;
+}
+
+.v-tab {
+  text-transform: none !important;
 }
 </style>
