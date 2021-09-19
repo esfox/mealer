27f597bed6432a28c2f9b4d4d7c4e9e2462a03d5
esfox <osairis100@gmail.com>
2021-09-19 22:02:13 +0800

    Update dialog content component

--- components/DialogContent.vue
+++ components/DialogContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card>
+  <v-card :max-height="maxHeight">
     <v-toolbar
       class="pr-0"
       v-bind="{
@@ -12,10 +12,10 @@
       <v-spacer />
       <v-btn icon :disabled="loading" @click="$emit('cancel')"><v-icon>mdi-close</v-icon></v-btn>
     </v-toolbar>
-    <v-card-text class="px-5" :class="{ 'mt-8': coloredTitleBar }">
+    <v-card-text class="px-5" :class="{ 'mt-6': coloredTitleBar }">
       <slot />
     </v-card-text>
-    <v-card-actions class="d-flex align-center pb-4 px-5">
+    <v-card-actions v-if="!noActions" class="d-flex align-center pb-4 px-5">
       <span v-if="error" class="error--text">{{ error }}</span>
       <v-spacer />
       <v-btn class="px-3" depressed large :disabled="loading" @click="$emit('cancel')">
@@ -47,9 +47,17 @@ export default {
       type: Boolean,
       default: false,
     },
+    noActions: {
+      type: Boolean,
+      default: false,
+    },
     isDanger: {
       type: Boolean,
       default: false,
+    },
+    maxHeight: {
+      type: String,
+      default: '',
     },
     loading: {
       type: Boolean,
