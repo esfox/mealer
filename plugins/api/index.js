3fd138af2e580f9c3fb86b6e2a4c06c140b86f06
esfox <osairis100@gmail.com>
2021-10-27 23:34:54 +0800

    Implement JWT checking and decoding

--- plugins/api/index.js
+++ plugins/api/index.js
@@ -2,8 +2,18 @@ import { foodAPI } from './food';
 import { mealsAPI } from './meals';
 import { mealTimesAPI } from './mealTimes';
 
-export default (context, inject) =>
+export default async (context, inject) =>
 {
+  const { auth } = context.$supabase;
+  const session = await auth.session();
+  if(session) context.$axios.setToken(session.access_token, 'Bearer');
+
+  auth.onAuthStateChange((event, newSession) =>
+  {
+    if(event === 'SIGNED_IN' && newSession)
+      context.$axios.setToken(newSession.access_token, 'Bearer');
+  });
+
   const food = foodAPI(context);
   const meals = mealsAPI(context);
   const mealTimes = mealTimesAPI(context);
