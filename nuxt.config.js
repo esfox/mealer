7229a799256977cee567d931b231e976219de291
esfox <osairis100@gmail.com>
2021-10-30 22:33:23 +0800

    Update meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,15 +5,46 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mealer',
+    title: 'Mealer',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'description', name: 'description', content: 'Very simple app to plan your meals' },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: process.env.BASE_URL || '',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Mealer',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Very simple app to plan your meals',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: `${process.env.BASE_URL || ''}/bentobox.png`,
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'mealer.vercel.app',
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website',
+      },
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
